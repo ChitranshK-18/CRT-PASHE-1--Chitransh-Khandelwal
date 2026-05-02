e91a2692a09f22081aa05438e9211dc2318c216e
Chitransh Khandelwal <chintugupta.200529@gmail.com>
2026-05-02 10:21:28 +0530

    Add switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char x;
+    cout<<"welcome to jecrc customer service...press a number:";
+    cin>>x;
+
+    switch(x){
+        case'1':
+        cout<<"for support";
+        break;
+        case'2':
+        cout<<"for billing";
+        break;
+        case'3':
+        cout<<"for technical support";
+        break;
+        case'4':
+        cout<<"speak to an agent";
+        case'0':
+        cout<<"speak to an agent";
+        break;
+
+        default:
+        cout<<"invalid input";
+        
+
+    }
+        return 0;
+}
