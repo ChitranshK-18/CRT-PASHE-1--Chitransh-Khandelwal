c1ef45bf466f6b9c88ab87d788a888511993706d
Chitransh Khandelwal <chintugupta.200529@gmail.com>
2026-05-05 11:13:12 +0530

    Add coffee.cpp

---
+++ functions/pointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 6;
+    int *ptr = &x;
+
+    cout<<ptr<<endl;
+
+    return 0;
+}
+
---
+++ functions/type.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void sum(int a, int b){
+    cout<<"The Sum is:"<<a+b<<endl;
+}
+
+int main(){
+
+    
+}
+
